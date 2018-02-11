15d9da54bd4c5cc82f9d4063de823de2171a6d01
antoinechalifour <antoine.chalifour@gmail.com>
2018-02-11 16:16:06 +0100

    feat(DownloadProgress): Add basic style

--- src/components/DownloadProgress/DownloadProgress.tsx
+++ src/components/DownloadProgress/DownloadProgress.tsx
@@ -1,5 +1,44 @@
+import * as React from 'react';
+import styled from 'styled-components';
 import { connect } from '../StateProvider';
 import { Download, StateContainer } from '../../core/State';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 12px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, .24);
+  background: #fff;
+`;
+
+const FileName = styled.div``;
+
+const ProgressWrapper = styled.div`
+  position: relative;
+  height: 10px;
+  flex: 1;
+  margin-left: 12px;
+  margin-right: 12px;
+`;
+
+const ProgressBar = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  background: green;
+`;
+
+const ProgressValue = styled.div`
+  font-family: monospace;
+  text-align: right;
+  font-feature-settings: "tnum";
+  font-variant-numeric: tabular-nums;
+`;
 
 export interface DownloadProgressProps {
   download?: Download;
@@ -10,9 +49,17 @@ export function DownloadProgress({ download }: DownloadProgressProps) {
     return null;
   }
 
-  console.log(`Download progress: ${(download.bytes / download.file.size * 100).toFixed(2)}%`);
+  const percent = download.bytes / download.file.size * 100;
 
-  return null;
+  return (
+    <Wrapper>
+      <FileName>{download.file.name}</FileName>
+      <ProgressWrapper>
+        <ProgressBar style={{ width: `${percent}%` }} />
+      </ProgressWrapper>
+      <ProgressValue>{percent.toFixed(2)}%</ProgressValue>
+    </Wrapper>
+  );
 }
 
 const mapStateToProps = (state: StateContainer): DownloadProgressProps => ({
